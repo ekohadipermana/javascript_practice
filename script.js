35a9d15cffbf6381ca20988bdaf87c608573b615
ekohadipermana <ekohadipermana@gmail.com>
2024-02-22 20:32:40 +0700

    practice variable and const

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // basic about number, got it
+// basic about variable
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
+
+// basic about constant
+const boilingPointC = 100;
+const boilingPointF = 212;
+
+console.log(myLuckyNumber, octopusLimbs, boilingPointC, boilingPointF);
