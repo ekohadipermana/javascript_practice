9858dd58d8585485e7f654af7695ae47b9fac1c6
ekohadipermana <ekohadipermana@gmail.com>
2024-02-23 19:25:26 +0700

    practice function

--- script.js
+++ script.js
@@ -31,3 +31,20 @@ const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
 let roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}`;
 alert(roll);
 
+/* Let's get some practice with conditional statements!  In order to make your 
+code repeatable and testable, I'm asking you to write your code inside a pre-defined 
+function (something we have not yet covered in the course).   
+Write your code between the two comments in index.js
+You will automatically have access to a variable called num.  Please do not try and 
+define num or change num in any way! I will be setting the value of num when I test 
+your code, so that I can test different outcomes
+If num is an even number, print out "even".  Don't do anything if num is an odd number.*/
+
+function isEven(num) {
+    if (num % 2 === 0) {
+        console.log('Even')
+        alert('Even')
+    }
+}
+
+isEven(8);
