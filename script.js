1e9923fb035b3c8c07d19c59bc0e97eac2dfea0d
ekohadipermana <ekohadipermana@gmail.com>
2024-02-23 19:13:58 +0700

    practice string literal

--- script.js
+++ script.js
@@ -25,3 +25,9 @@ const word = "skateboard";
 let facialHair = word.slice(5).replace('o', 'e');
 console.log(facialHair);
 
+// string literal
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+let roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}`;
+alert(roll);
+
